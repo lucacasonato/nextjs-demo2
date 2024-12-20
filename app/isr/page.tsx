3358a2cc8833767066abdc1f12eb8e40b77c2a9d
Luca Casonato <hello@lcas.dev>
2024-12-20 11:21:09 +0100

    Add ISR page

---
+++ app/isr/page.tsx
@@ -0,0 +1,23 @@
+import { revalidateTag, unstable_cache } from "next/cache"
+
+const getCurrentTime = unstable_cache(
+  async () => {
+    return new Date().toISOString()
+  },
+  ['time'],
+  { tags: ['time'] }
+)
+
+
+export default async function Page() {
+  async function invalidateCache() {
+    'use server'
+    revalidateTag("time")
+  }
+
+  const time = await getCurrentTime()
+  return <div>
+    <p>Current time: {time}</p>
+    <button onClick={invalidateCache}>Invalidate cache</button>
+  </div>
+}
